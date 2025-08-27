7c34358a92c2987aeae5b23a894fc9ba785386bb
vlad_ermakov <maxvel1001@gmail.com>
2025-08-27 19:55:54 +0300

    Homework

---
+++ lesson_2/homework_2.py
@@ -0,0 +1,64 @@
+a = 8
+b = 3
+x = 2.5
+y = -1.7
+print(type(a), type(b), type(x), type(y))
+num1 = 7
+num2 = 8
+print(num1 + num2, num1 - num2, num1 * num2, num1 / num2, num1 // num2, num1 % num2, num1 ** num2)
+a = 10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+a = -10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+a = 10
+b = -3
+print(a / b)
+print(a // b)
+print(a % b)
+print(5 + 3 * 2)
+print((5 + 3) * 2)
+print(10 / 2 ** 2)
+count = 10
+count += 5
+count -= 3
+count *= 2
+count /= 4
+print(count)
+s1 = "Python"
+s2 = 'Программирование'
+print(s1, s2)
+s3 = '''Курсы пайтон просто класс, и это не конец! Я пишу длинную строку и она будет на несколько строк'''
+print(s3)
+print(len(''))
+first_name = "Иван"
+last_name = "Петров"
+full_name  = first_name + " " + last_name
+print(full_name)
+s = "Возраст: "
+age = 25
+print(s + str(age))
+print("ха" * 5)
+text = "Привет, мир!"
+print(len(text))
+x = ''
+print(len(x))
+sentence = "Я изучаю Python"
+print('Python' in sentence)
+print('Java' in sentence)
+a = "apple"
+b = "banana"
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+print(ord('А'))
+print(ord('а'))
+print(ord('Я'))
