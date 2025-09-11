0f4f146255e1d0080ad65c12dd1adb3c4616ff78
vlad_ermakov <maxvel1001@gmail.com>
2025-09-11 14:17:49 +0300

    Homework

---
+++ Course/L34 - функции.py
@@ -0,0 +1,38 @@
+# Задание 1:
+
+# def greet(name):
+#     text = f"Привет, {name}! Добро пожаловать!"
+#     print(text)
+
+# greet("Анна")
+
+# Задание 2:
+
+# def square(num):
+#     a = num ** 2
+#     print(a)
+
+# square(5)
+
+# Задание 3:
+
+# def sum_numbers(a, b):
+#     c = a + b
+#     print(c)
+
+# sum_numbers(3, 7)
+
+# Задание 4:
+
+# def is_even(num):
+#     return "True" if num % 2 == 0 else "False"
+#
+# print(is_even(4))
+# print(is_even(7))
+
+# Задание 5:
+
+def repeat_string(text, times):
+    return text * times
+
+print(repeat_string("Python", 3))
